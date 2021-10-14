bad78802bf5869e830ebe0788d95437d0ba711c5
basantiiitk <88549976+basantiiitk@users.noreply.github.com>
2021-10-14 21:29:16 +0530

    Update search_ordered_list.py

--- Chapter10/search_ordered_list.py
+++ Chapter10/search_ordered_list.py
@@ -1,14 +1,30 @@
-def search_ordered(ordered_list, term):  
-    ordered_list_size = len(ordered_list)  
-    for i in range(ordered_list_size):  
-        if term == ordered_list[i]:  
-            return i  
-        elif ordered_list[i] > term:  
-            return None  
-    return None  
-  
-list1 = [1, 3, 6, 11, 88, 100, 110, 150]
 
-print(search_ordered(list, 11))
+def search(ordered_list, term):
+    ordered_list_size = len(ordered_list)
+    for i in range(ordered_list_size):
+        if term == ordered_list[i]:
+            return i
+        elif ordered_list[i] > term:
+            return None
 
-print(search_ordered(list, 101))
+    return None
+
+
+
+scores = [2, 3, 4, 6, 7]
+
+search_term = 5
+position = search(scores, search_term)
+
+if position is None:
+    print("{} not found".format(search_term))
+else:
+    print("{} found at position {}".format(search_term, position))
+
+
+search_term = 2
+position = search(scores, search_term)
+if position is None:
+    print("{} not found".format(search_term))
+else:
+    print("{} found at position {}".format(search_term, position))
