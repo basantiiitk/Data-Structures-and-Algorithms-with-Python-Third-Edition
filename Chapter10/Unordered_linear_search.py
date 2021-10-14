f3c9a1371877114e56a0318526e77fb872634514
basantiiitk <88549976+basantiiitk@users.noreply.github.com>
2021-10-14 21:28:21 +0530

    Update Unordered_linear_search.py

--- Chapter10/Unordered_linear_search.py
+++ Chapter10/Unordered_linear_search.py
@@ -1,13 +1,27 @@
-def search(unordered_list, term):  
-    unordered_list_size = len(unordered_list)  
-    for i in range(unordered_list_size):  
-        if term == unordered_list[i]:  
-            return i  
-    return None  
-  
-list = [60, 1, 88, 10, 11, 100]
 
-print(search(list, 10))
+def search(unordered_list, term):
+    unordered_list_size = len(unordered_list)
+    for i in range(unordered_list_size):
+        if term == unordered_list[i]:
+            return i
+    return None
 
-print(search(list, 103))
-  
+
+
+scores = [60, 1, 88, 10, 11, 600]
+
+search_term = 65
+position = search(scores, search_term)
+
+if position is None:
+    print("{} not found".format(search_term))
+else:
+    print("{} found at position {}".format(search_term, position))
+
+
+search_term = 600
+position = search(scores, search_term)
+if position is None:
+    print("{} not found".format(search_term))
+else:
+    print("{} found at position {}".format(search_term, position))
